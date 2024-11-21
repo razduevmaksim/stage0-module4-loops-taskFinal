bfc0a1b964fe7de66608956134a1d464f4de55b6
Maksim <mrazduev@gmail.com>
2024-11-21 18:23:07 +0300

    Module 4.2. Final Task

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,21 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = cathetusLength; j > i; j--) {
+                System.out.print(" ");
+            }
 
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
